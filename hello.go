b34be68bb0ef5c014fb32bcbf24d9ae9d964d36e
Folombas <folombas@yandex.ru>
2025-10-18 16:33:10 +0000

    Added thirdNumber

--- hello.go
+++ hello.go
@@ -3,11 +3,13 @@ package main
    import "fmt"
 
 func main()  {
-	var firstNumber int = 5
-	var secondNumber int = 6
+	var firstNumber int = 10
+	var secondNumber int = 3
+	var thirdNumber int = 7
 	a := firstNumber
 	b := secondNumber
-	sum := a + b
+	c := thirdNumber
+	sum := a + b + c
 	fmt.Println(sum)
 }
 
