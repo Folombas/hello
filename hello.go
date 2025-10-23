688c061b2d143bddcaca80e6c7fa0564a9305e0b
Folombas <folombas@yandex.ru>
2025-10-23 05:46:13 +0000

    1000

--- hello.go
+++ hello.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
     counter := 1
-    for counter < 100 { // Цикл выполняется, пока counter меньше 10
+    for counter < 1000 { // Цикл выполняется, пока counter меньше 1000
         fmt.Println("Счетчик:", counter)
         counter *= 2 // Увеличиваем значение counter
     }
