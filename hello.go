df00c4b26d9b0c76a60a95a918e6018d798dcf63
Folombas <folombas@yandex.ru>
2025-10-27 12:57:00 +0000

    feat: implement doubling until exceeding 1000

--- hello.go
+++ hello.go
@@ -3,37 +3,10 @@ package main
 import "fmt"
 
 func main() {
-    var a, b float64
-    var operation string
-
-    // Считываем первое число
-    fmt.Scan(&a)
-
-    // Считываем символ операции
-    fmt.Scan(&operation)
-
-    // Считываем второе число
-    fmt.Scan(&b)
-
-    // Обрабатываем операцию с помощью switch
-    switch operation {
-    case "+":
-        result := a + b
-        fmt.Printf("%.2f\n", result)
-    case "-":
-        result := a - b
-        fmt.Printf("%.2f\n", result) // Убрал лишнюю строку с "x"
-    case "*":
-        result := a * b
-        fmt.Printf("%.2f\n", result)
-    case "/":
-        if b == 0 {
-            fmt.Println("Деление на ноль!")
-        } else {
-            result := a / b
-            fmt.Printf("%.2f\n", result)
-        }
-    default:
-        fmt.Println("Неизвестная операция")
+    result := 1
+    
+    for result <= 1000 {
+        fmt.Println(result)
+        result *= 2
     }
 }
